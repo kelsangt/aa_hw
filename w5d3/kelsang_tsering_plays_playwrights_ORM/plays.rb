af81c704c91d92e4730b1dcf90f7d4c4c2e56778
“Kelsang” <kelsangrtsering@gmail.com>
2023-02-08 07:29:13 -0500

    Work on homework for w5d3

--- w5d3/kelsang_tsering_plays_playwrights_ORM/plays.rb
+++ w5d3/kelsang_tsering_plays_playwrights_ORM/plays.rb
@@ -14,10 +14,33 @@ end
 class Play
   attr_accessor :id, :title, :year, :playwright_id
 
+  def self.find_by_title(title)
+    PlayDBConnection.instance.execute(<<-SQL, @title)
+      SELECT
+        title
+      FROM
+        plays
+      WHERE
+        title = ?
+    SQL
+  end
+
+  def self.find_by_playwright(name)
+    PlayDBConnection.instance.execute(<<-SQL, @playwrights.name)
+      SELECT 
+        *
+      JOIN plays
+        ON plays.playwright_id = playwrights.id
+      WHERE
+        name = ?
+    SQL
+  end
+
   def self.all
     data = PlayDBConnection.instance.execute("SELECT * FROM plays")
     data.map { |datum| Play.new(datum) }
   end
+
 
   def initialize(options)
     @id = options['id']
@@ -44,6 +67,55 @@ class Play
         plays
       SET
         title = ?, year = ?, playwright_id = ?
+      WHERE
+        id = ?
+    SQL
+  end
+end
+
+class Playwright 
+  attr_accessor :id, :name, :birth_year
+  
+  def self.all 
+    info = PlayDBConnection.instance.execute("SELECT * FROM playwrights")
+    info.map { |information| Playwright.new(information) }
+  end
+
+  def self.find_by_name(name)
+    PlayDBConnection.instance.execute(<<-SQL, @name)
+      SELECT
+        name
+      FROM
+        playwrights
+      WHERE 
+        name = ?
+    SQL
+  end
+
+  def initialize(hash)
+    @id = hash['id']
+    @name = hash['name']
+    @birth_year = hash['birth_year']
+  end
+
+  def create
+    raise "#{self} already in database" if self.id
+    PlayDBConnection.instance.execute(<<-SQL, self.name, self.birth_year)
+      INSERT INTO
+        playwrights (name, birth_year)
+      VALUES
+        (?, ?)
+    SQL
+    self.id = PlayDBConnection.instance.last_insert_row_id
+  end
+
+  def update
+    raise "#{self} not in database" unless self.id
+    PlayDBConnection.instance.execute(<<-SQL, self.name, self.birth_year, self.id)
+      UPDATE
+        playwrights
+      SET
+        name = ?, birth_year = ?
       WHERE
         id = ?
     SQL
