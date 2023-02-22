f5051007ecdcc2b4a82b1e2d5924bbcec8286d55
“Kelsang” <kelsangrtsering@gmail.com>
2023-02-22 07:22:37 -0500

    Work on homework for w7d3

---
+++ w7d3/practice-for-ch-rails-capybara-main/spec/models/user_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:password_digest) }
+  it do
+    should validate_length_of(:password).
+      is_at_least(6).
+      on(:create)
+  end
+end
