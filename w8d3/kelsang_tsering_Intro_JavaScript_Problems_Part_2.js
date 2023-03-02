476e9b8a19ea1bc854583bf1426d24a0b47cbdeb
“Kelsang” <kelsangrtsering@gmail.com>
2023-03-02 07:20:31 -0500

    work on homework for w8d3

---
+++ w8d3/kelsang_tsering_Intro_JavaScript_Problems_Part_2.js
@@ -0,0 +1,13 @@
+function printCallback(array) {
+    array.forEach((ele) => console.log(ele));
+}
+
+function titleize(names, callback) {
+   let newArr = [];
+   names.map(name => {
+    newArr.push("Mx. " + name + " Jingleheimer " + "Schmidt");
+   })
+   callback(newArr);
+}
+
+titleize(["Mary", "Brian", "Leo"], printCallback);
